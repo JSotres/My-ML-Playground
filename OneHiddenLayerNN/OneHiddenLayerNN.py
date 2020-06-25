03f7db4e35b51ff7c958344ebf37e7cdc3a23691
Javier Sotres <jsotres@gmail.com>
2020-06-25 07:02:13 +0200

    several bugs fixed

--- OneHiddenLayerNN/OneHiddenLayerNN.py
+++ OneHiddenLayerNN/OneHiddenLayerNN.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class OneHiddenLayerNN():
@@ -16,13 +17,14 @@ class OneHiddenLayerNN():
         self.params = {}
         self.cache = {}
         self.grads = {}
+        self.costs = []
 
     def initialize(self):
         np.random.seed(1)
         self.params['W1'] = np.random.randn(self.n_hidden_neurons, self.n_features)*0.01
         self.params['b1'] = np.zeros((self.n_hidden_neurons, 1))
         self.params['W2'] = np.random.randn(self.n_output_neurons, self.n_hidden_neurons)*0.01
-        self.params['b2'] = np.zeros((n_output_neurons, 1))
+        self.params['b2'] = np.zeros((self.n_output_neurons, 1))
 
     @staticmethod
     def sigmoid(z):
@@ -35,22 +37,21 @@ class OneHiddenLayerNN():
         self.cache['Z1'] = np.dot(self.params['W1'],X)+self.params['b1']
         self.cache['A1'] = np.tanh(self.cache['Z1'])
         self.cache['Z2'] = np.dot(self.params['W2'],self.cache['A1'])+self.params['b2']
-        self.cache['A2'] = sigmoid(self.cache['Z2'])
+        self.cache['A2'] = self.sigmoid(self.cache['Z2'])
+        #print(self.cache['A2'].shape)
+        #print(self.cache['A2'])
 
     def compute_cost(self, y):
         m = y.shape[1] # number of example
         # Compute the cross-entropy cost
-        logprobs = np.multiply(
-            np.log(self.cache['A2']),y)+np.multiply(np.log(1-self.cache['A2']),
-            (1-y)
-            )
+        logprobs = np.multiply(np.log(self.cache['A2']), y) + np.multiply(np.log(1-self.cache['A2']), (1-y))
         cost = -1/m*np.sum(logprobs)
         cost = float(np.squeeze(cost))  # makes sure cost is the dimension we expect. 
         return cost
 
-    def backward_propagation(self, X, Y):
+    def backward_propagation(self, X, y):
         m = X.shape[1]
-        dZ2 = A2-Y
+        dZ2 = self.cache['A2']-y
         self.grads['dW2'] = 1/m*np.dot(dZ2,self.cache['A1'].T)
         self.grads['db2'] = 1/m*np.sum(dZ2, axis=1, keepdims=True)
         dZ1 = np.multiply(np.dot(self.params['W2'].T,dZ2),(1 - np.power(self.cache['A1'], 2)))
@@ -58,18 +59,18 @@ class OneHiddenLayerNN():
         self.grads['db1'] = 1/m*np.sum(dZ1, axis=1, keepdims=True)
 
     def update_parameters(self, learning_rate):
-        self.params['W1'] = learning_rate*self.grads['dW1']
-        self.params['b1'] = learning_rate*self.grads['db1']
-        self.params['W2'] = learning_rate*self.grads['dW2']
-        self.params['b2'] = learning_rate*self.grads['db2']
+        self.params['W1'] -= learning_rate*self.grads['dW1']
+        self.params['b1'] -= learning_rate*self.grads['db1']
+        self.params['W2'] -= learning_rate*self.grads['dW2']
+        self.params['b2'] -= learning_rate*self.grads['db2']
 
-    def train(X, y, num_iterations = 10000, learning_rate = 1.2, print_cost=False):
+    def train(self, X, y, print_cost=False, num_iterations = 5000, learning_rate = 1.2):
         self.n_features = X.shape[0]
         self.n_samples = X.shape[1]
         self.n_output_neurons = y.shape[0]
         
         # Initialize parameters
-        self.params = self.initialize()
+        self.initialize()
 
         for i in range(0, num_iterations):
          
@@ -77,7 +78,7 @@ class OneHiddenLayerNN():
             self.forward_propagation(X)
             
             # Compute Cost function.
-            cost = compute_cost(y)
+            cost = self.compute_cost(y)
      
             # Backpropagation. 
             self.backward_propagation(X, y)
@@ -86,24 +87,35 @@ class OneHiddenLayerNN():
             self.update_parameters(learning_rate)
             
             # Print the cost every 1000 iterations
-            if print_cost and i % 1000 == 0:
+            if print_cost and i % 100 == 0:
                 print ("Cost after iteration %i: %f" %(i, cost))
+                self.costs.append(cost)
 
     def predict(self, X):
         Z1 = np.dot(self.params['W1'],X)+self.params['b1']
         A1 = np.tanh(Z1)
         Z2 = np.dot(self.params['W2'],A1)+self.params['b2']
-        A2 = sigmoid(Z2)
+        A2 = self.sigmoid(Z2)
+        y_predicted = np.zeros((1, A2.shape[1]))
         for i in range(A2.shape[1]):
-            y_prediction[0,i] = A2[0,i] > 0.5
+            y_predicted[0,i] = A2[0,i] > 0.5
 
-        return y_prediction
+        return y_predicted
 
     def scoring(self, y_predicted, y_true):
         """
         Returns accuracy
         """
         return((y_predicted == y_true).sum()/y_predicted.shape[1])
+
+    def plotCosts(self):
+        # Plot learning curve (with costs)
+        costs = np.squeeze(self.costs)
+        plt.plot(costs)
+        plt.ylabel('cost')
+        plt.xlabel('iterations/100')
+        plt.title("Cost Function")
+        plt.show()
     
 
     
